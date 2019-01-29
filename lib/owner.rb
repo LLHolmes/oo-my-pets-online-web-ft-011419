23fac7b6634dc83bb9d063aaf5304c1fe4afbd3c
Lana Holmes <lana.lee.holmes@gmail.com>
2019-01-29 15:14:00 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,9 +1,10 @@
 class Owner
   attr_accessor :name
-  attr_reader :species
+  attr_reader :species, :pets
   
   def initialize(species)
     @species =  species
+    @pets = {fishes: [], cats: [], dogs: []}
   end
   
 end
