51e56472213be831cef66191ba68737ff8863cc5
Lana Holmes <lana.lee.holmes@gmail.com>
2019-01-29 15:18:15 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -10,10 +10,37 @@ class Owner
   def buy_fish
   end
  
-  def buy_fish
+  def buy_cat
   end
  
-  def buy_fish
+  def buy_dog
+  end
+ 
+  def walk_dogs
+  end
+ 
+  def play_with_cats
+  end
+ 
+  def feed_fish
+  end
+ 
+  def sell_pets
+  end
+ 
+  def list_pets
+  end
+ 
+  def self.all
+    @@all
+  end
+ 
+  def self.count
+    @@all.length
+  end
+ 
+  def self.reset_all
+    @@all.clear
   end
 
 end
